2991c9d23b9712dd883c71781b4092957908e940
denysprokhorin <prokhorind@gmail.com>
2024-09-17 14:43:18 +0300

    add if examples

---
+++ python/conditions/if.py
@@ -0,0 +1,17 @@
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+x = 3
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is 5 or less")
+
+x = 7
+if x > 10:
+    print("x is greater than 10")
+elif x > 5:
+    print("x is greater than 5 but less than or equal to 10")
+else:
+    print("x is 5 or less")
