c9318862eec32a06d197ceb43acb738b96a53f99
denysprokhorin <prokhorind@gmail.com>
2024-10-03 19:57:51 +0300

    add bubble sort example

---
+++ python/code_quality/actual_code_for_buble_sort_task.py
@@ -0,0 +1,15 @@
+def bubble_sort(numbers):
+    for i in range(len(numbers)):
+        for j in range(0, len(numbers) - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return numbers
+
+
+
+if __name__ == "__main__":
+    unsorted_list = [64, 34, 25, 12, 22, 11, 90]
+
+    sorted_list = bubble_sort(unsorted_list)
+
+    print(f"Відсортований список: {sorted_list}")
