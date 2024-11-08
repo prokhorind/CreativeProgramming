db1429932761f1b41c0d36ddfe086a7f34dd64a5
denysprokhorin <prokhorind@gmail.com>
2024-11-08 12:00:52 +0200

    modify examples

--- python/arrays/max.py
+++ python/arrays/max.py
@@ -5,3 +5,14 @@ numbers = [8, 3, 6, 12, 7]
 max_number = max(numbers)
 
 print("Максимальне значення у списку:", max_number)
+
+
+# Ініціалізація змінної для максимального значення
+max_number = numbers[0]  # Припустимо, що перший елемент - найбільший
+
+# Цикл для знаходження максимального значення
+for num in numbers:
+    if num > max_number:
+        max_number = num
+
+print("Максимальне значення у списку:", max_number)
--- python/arrays/sum.py
+++ python/arrays/sum.py
@@ -5,3 +5,12 @@ numbers = [5, 10, 15]
 sum_of_numbers = sum(numbers)
 
 print("Сума елементів у списку:", sum_of_numbers)
+
+# Ініціалізація змінної для суми
+sum_of_numbers = 0
+
+# Цикл для обчислення суми елементів
+for num in numbers:
+    sum_of_numbers += num
+
+print("Сума елементів у списку:", sum_of_numbers)
