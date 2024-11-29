4042db3c112005da0cd35db9aebe557da2cd0805
denysprokhorin <prokhorind@gmail.com>
2024-11-29 12:17:26 +0200

    add templates for task

---
+++ python/functions/modules/task/even_template.py
@@ -0,0 +1,10 @@
+# Площа прямокутника
+width = float(input("Введіть ширину прямокутника: "))
+height = float(input("Введіть висоту прямокутника: "))
+rectangle_area = width * height
+print(f"Площа прямокутника: {rectangle_area}")
+
+# Площа кола
+radius = float(input("Введіть радіус кола: "))
+circle_area = 3.14159 * radius ** 2
+print(f"Площа кола: {circle_area}")
